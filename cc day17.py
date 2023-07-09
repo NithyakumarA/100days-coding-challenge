7d19f4872fe04a800e77e900e0240e25d627f44a
NithyakumarA <134161275+NithyakumarA@users.noreply.github.com>
2023-07-09 11:33:05 +0530

    Add files via upload

---
+++ cc day17.py
@@ -0,0 +1,16 @@
+# cook your dish here
+'''
+Problem
+Write a program that takes a number 
+N as the input, and prints it to the output.
+
+Input Format:
+The only line of input contains a single integer.
+
+Output Format:
+Output the answer in a single line.
+    '''
+n=int(input())
+i=100000
+if(n>0 and n<=i):
+    print(n)
